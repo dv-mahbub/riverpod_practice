7a47b6ba1420afa735d2f0f96657312eaf45d03c
dv-mahbub <dv.mahbub@gmail.com>
2024-12-30 12:19:37 +0600

    future provider example

---
+++ lib/8_types_provider/future_provider.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+final numberFutureProvider = FutureProvider<int>((ref) async {
+  await Future.delayed(const Duration(seconds: 2));
+  return 33;
+});
+
+class FutureProviderExample extends StatelessWidget {
+  const FutureProviderExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer(
+              builder: (__, WidgetRef ref, _) {
+                return ref.watch(numberFutureProvider).when(
+                    data: (data) => Text('The number is: $data'),
+                    error: (error, stackTrace) {
+                      Logger().e(error);
+                      return Text(
+                          'Error: $error, Stack trace: ${stackTrace.toString()}');
+                    },
+                    loading: () => const CircularProgressIndicator());
+              },
+            ),
+            FilledButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        const FutureProviderExample2ndPage()));
+              },
+              child: const Text('Go'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FutureProviderExample2ndPage extends StatelessWidget {
+  const FutureProviderExample2ndPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer(
+              builder: (__, WidgetRef ref, _) {
+                return ref.watch(numberFutureProvider).when(
+                    data: (data) => Text('The number is: $data'),
+                    error: (error, stackTrace) {
+                      Logger().e(error);
+                      return Text(
+                          'Error: $error, Stack trace: ${stackTrace.toString()}');
+                    },
+                    loading: () => const CircularProgressIndicator());
+              },
+            ),
+            FilledButton(
+              style: FilledButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_practice/8_types_provider/state_notifier_provider2.dart';
+import 'package:riverpod_practice/8_types_provider/future_provider.dart';
 
 void main() {
   runApp(
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: StateNotifierProviderExample2(),
+      home: FutureProviderExample(),
     );
   }
 }
